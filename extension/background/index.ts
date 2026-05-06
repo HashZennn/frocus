7ee33b5ace6158797f837b70de68c31bd2b103ce
AD <burningggflamesss@gmail.com>
2026-05-06 13:57:48 +0545

    feat: add types and done some setups for the bigger execution

--- extension/background/index.ts
+++ extension/background/index.ts
@@ -1,10 +1,20 @@
 import { Storage } from "@plasmohq/storage"
+import { extractPageMeta } from "../contents/parser";
 
 export {}
 
 class FrocusTracker {
     private storage = new Storage()
     
+    test() {
+        chrome.tabs.onActivated.addListener(async ({ tabId }) => {
+            const tab = await chrome.tabs.get(tabId)
+
+            console.log("FROCUS: ", tab.active, ". DETAILS: ", extractPageMeta([]))
+        })
+    }
+
 }
 
-new FrocusTracker()
+const frocus = new FrocusTracker()
+frocus.test()
